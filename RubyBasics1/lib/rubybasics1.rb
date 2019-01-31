6d2f8a97b9c6c5ea5c9c02aac2c82337956155bf
Jack Hitchcock <Jhitchc5@uncc.edu>
2019-01-31 15:32:31 +0000

    RubyBasics1 -Part 1

--- RubyBasics1/lib/rubybasics1.rb
+++ RubyBasics1/lib/rubybasics1.rb
@@ -1,15 +1,35 @@
 # Lab 1
 # Part I
 def sum arr
-  # YOUR CODE HERE
+  (!arr.nil? and arr.any?) ? arr.inject(0, :+) : 0  
 end
 
 # Part II
 def max_2_sum arr
-  # YOUR CODE HERE
+if arr.nil? or arr.empty?
+    0
+  elsif arr.length == 1
+    arr.first
+  else
+    max = arr.sort[-2..-1]
+    max.first + max.last
+end
 end
 
 # Part III
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  #   return false if arr.nil? or arr.empty? or arr.length == 1
+  arr.each do |first|
+    arr.each do |second|
+      return true if (first + second == n) and first != second
+    end
+  end
+  false
+  return false if arr.nil? or arr.empty? or arr.length == 1
+  arr.each do |first|
+    arr.each do |second|
+      return true if (first + second == n) and first != second
+    end
+  end
+  false
 end
