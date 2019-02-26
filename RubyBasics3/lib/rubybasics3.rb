88490d2bf88376d7d3d2ebbe9e24fc625b95bbea
Jack Hitchcock <Jhitchc5@uncc.edu>
2019-02-26 17:35:30 +0000

    Ruby OO Basics - Part 3

--- RubyBasics3/lib/rubybasics3.rb
+++ RubyBasics3/lib/rubybasics3.rb
@@ -3,12 +3,14 @@
 class BookInStock
     attr_accessor :isbn, :price
 
-  def initialize (isbn, price)
+  def initialize isbn, price
     if isbn.empty? || price <= 0
         raise ArgumentError
     end
     @isbn = isbn
     @price = price
   end
-    
+    def price_as_string
+    format("$%.2f", @price)
+    end
 end
